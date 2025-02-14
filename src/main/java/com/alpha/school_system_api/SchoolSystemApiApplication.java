8a592d4900ffb1a70329483933111e0dfe9fd3fe
Wesley <147221712+Wesleyweb30@users.noreply.github.com>
2025-02-14 08:40:40 -0300

    First Commit

---
+++ src/main/java/com/alpha/school_system_api/SchoolSystemApiApplication.java
@@ -0,0 +1,14 @@
+package com.alpha.school_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSystemApiApplication.class, args);
+		System.out.println("Hello, world ");
+	}
+
+}
---
+++ src/test/java/com/alpha/school_system_api/SchoolSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.school_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
