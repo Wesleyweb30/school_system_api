60f2021caf9ff1810333a89544469b1dbdbee131
WesleyWeb30 <Wesleyweb3.0@gmail.com>
2025-05-31 16:01:59 -0300

    disabling school registration through the school controller

--- src/main/java/com/alpha/school_system_api/controller/EscolaController.java
+++ src/main/java/com/alpha/school_system_api/controller/EscolaController.java
@@ -1,18 +1,18 @@
 package com.alpha.school_system_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alpha.school_system_api.dtos.EscolaDTO;
-import com.alpha.school_system_api.model.Escola;
+
 import com.alpha.school_system_api.repository.EscolaRepository;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
@@ -22,10 +22,10 @@ public class EscolaController {
     @Autowired
     private EscolaRepository escolaRepository;
 
-    @PostMapping    
-    public Escola criar(@RequestBody Escola escola){
-        return escolaRepository.save(escola);
-    } 
+    // @PostMapping    
+    // public Escola criar(@RequestBody Escola escola){
+    //     return escolaRepository.save(escola);
+    // } 
 
     @GetMapping
     public List<EscolaDTO>listar(){
